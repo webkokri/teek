439a1a329274ff3d5727496bb7f7c7f919ee9c21
webkokri <59289573+webkokri@users.noreply.github.com>
2022-05-30 00:36:26 +0530

    my first commit

---
+++ client/src/components/Footer/SmallFooter/SmallFooter.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Icon from '../../Icon/Icon';
+
+const SmallFooter = () => {
+  const links = [
+    'About',
+    'Help',
+    'Press',
+    'API',
+    'Jobs',
+    'Privacy',
+    'Terms',
+    'Locations',
+    'Top Accounts',
+    'Hashtags',
+    'Language',
+  ];
+  return (
+    <footer className="footer--small color-grey-2 font-bold">
+      <div className="footer--small__links">
+        {links.map((link, idx) => (
+          <Link key={idx} to="/">
+            {link}
+          </Link>
+        ))}
+      </div>
+      <h5
+        className="heading-5 color-grey-2 footer--small__copyright mt-lg font-bold"
+        style={{ textTransform: 'uppercase ' }}
+      >
+        &copy; 2022 TEEKUH
+        
+      </h5>
+    </footer>
+  );
+};
+
+export default SmallFooter;
